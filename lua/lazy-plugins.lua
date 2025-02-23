c7480eec5ce049bfbc574770297398c28973f52e
mattfern <28821103+mattfern@users.noreply.github.com>
2025-02-23 19:32:53 +1100

    feat: update mini.nvim configuration and dependencies in lazy-lock.json

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -40,8 +40,6 @@ require('lazy').setup({
 
   require 'kickstart/plugins/todo-comments',
 
-  require 'kickstart/plugins/mini',
-
   require 'kickstart/plugins/treesitter',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
@@ -64,7 +62,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
